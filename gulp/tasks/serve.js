4d32a3e543cfba6e0ab80c2b4128607841d20a81
Grom <gromimol@ukr.net>
2021-04-26 15:56:50 +0300

    new-search

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function () {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "non-escort-member.html"
+                index: "dating-new-search.html"
             }
         });
     });
