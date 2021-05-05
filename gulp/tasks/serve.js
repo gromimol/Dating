ad6d269951731c937c51360bdf5d7a7e405f310a
Grom <gromimol@ukr.net>
2021-05-05 14:48:05 +0300

    faq

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function () {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "link-to-us.html"
+                index: "advertise.html"
             }
         });
     });
