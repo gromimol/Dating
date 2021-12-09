dcff97746ca572f77609b74195174797158f26e8
Grom <gromimol@ukr.net>
2021-12-09 17:56:31 +0200

    Photo raiting page

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "photo-raiting.html"
+                index: "add-raiting-to-photo.html"
             }
         });
     });
